4e9142ffb8334522251b0b7de0de0b7723105a52
北邙 <communism_1921@163.com>
2024-07-18 17:23:40 +0800

    配置文件

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,9 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+  base: './',
   plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': '/src'
-    }
+  build: {
+    outDir: 'dist'
   }
 });
